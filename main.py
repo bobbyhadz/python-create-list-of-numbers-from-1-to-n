c85a7b7a627af8fa46cafb8d6edacddc84ead6d8
bobbyhadz <opmftw@gmail.com>
2024-04-10 09:48:26 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Create a list of numbers from 1 to N in Python
+
+list_of_numbers = list(range(1, 6))
+
+print(list_of_numbers)  # 👉️ [1, 2, 3, 4, 5]
+
+list_of_numbers = list(range(1, 10, 2))
+print(list_of_numbers)  # 👉️ [1, 3, 5, 7, 9]
